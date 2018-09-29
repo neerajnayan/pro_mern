3bea82c13fd8765fed51bf0f69bee70a0c7dfcbe
Neeraj Nayan <neeraj.nayan@panasonic.co.in>
2018-09-29 10:16:53 +0530

    Chapter-3: React Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,95 @@
 
 var contentNode = document.getElementById('contents');
 
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-const message = continents.map(c => `Hello ${c}!`).join(' ');
+class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the issue filter</div>
+    );
+  }
+}
 
-var component = <p>{message}</p>;
-ReactDOM.render(component, contentNode); // Render the component inside the content Node
+class IssueRow extends React.Component {
+  render() {
+    const issue = this.props.issue;
+    return (
+      <tr>
+        <td>{issue.id}</td>
+        <td>{issue.status}</td>
+        <td>{issue.owner}</td>
+        <td>{issue.created.toDateString()}</td>
+        <td>{issue.effort}</td>
+        <td>{issue.completionDate ? issue.completionDate.toDateString() : ''}</td>
+        <td>{issue.title}</td>
+      </tr>
+    );
+  }
+}
+
+IssueRow.propTypes = {
+    issue_id: React.PropTypes.number.isRequired,
+    issue_title: React.PropTypes.string
+};
+
+class IssueTable extends React.Component {
+  render() {
+    const issueRows = this.props.issues.map(issue => 
+        <IssueRow key={issue.id} issue={issue} />);
+    return (
+      <table className='bordered-table'>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Status</th>
+                    <th>Owner</th>
+                    <th>Created</th>
+                    <th>Effort</th>
+                    <th>Completion Date</th>
+                    <th>Title</th>
+                </tr>
+            </thead>
+            <tbody>
+                {issueRows}
+            </tbody>
+      </table>
+    );
+  }
+}
+
+class IssueAdd extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for an issue Add Entry form.</div>
+    );
+  }
+}
+
+const issues = [
+    {
+        id: 1, status: 'Open', owner: 'Ravan',
+        created: new Date('2016-08-05'), effort: 5, completionDate: undefined,
+        title: 'Error in console while clicking Add'
+    },
+    {
+        id: 2, status: 'Assigned', owner: 'Eddie',
+        created: new Date('2018-09-22'), effort: 14, completionDate: new Date('2018-10-10'),
+        title: 'Missing bottom border on panel'
+    }
+];
+
+class IssueList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable issues={issues} />
+        <hr />
+        <IssueAdd />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<IssueList />, contentNode);
